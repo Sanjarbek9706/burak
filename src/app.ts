e6797737bd367279e4f718df08df585921bf1002
Sanjarbek9706 <san19970618@gmail.com>
2026-05-27 18:43:51 +0900

    feat: create routing and member controller

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from "path";
+import router from "./router";
 
 /* 1-ENTRANCE */
 const app = express();
@@ -15,6 +16,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set("view enginer", "ejs");
 
 /* 4-ROUNTERS */
-
+app.use("/", router);  //Meddleware Design patter
 
 export default app;
