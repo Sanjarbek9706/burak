468fca9e1de898f2d2424639a0da6cbed277718d
Sanjarbek9706 <san19970618@gmail.com>
2026-06-06 20:26:33 +0900

    fix: create sessions on login  and signup process

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,11 @@
- import dotenv from 'dotenv'; 
- dotenv.config();
+ import 'dotenv/config';
  import mongoose from 'mongoose';
  import app from "./app";
-
 
  mongoose
  .connect(process.env.MONGO_URL as string, {})
  .then((data) => {
-    console.log('MongoDB  connection secceed');
+    console.log('MongoDB connection secceed');
     const PORT = process.env.PORT ?? 3003;
     app.listen(PORT, function () {
         console.info(`The server is running successfully on port: ${PORT}`);
