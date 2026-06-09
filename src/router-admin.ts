f793f05f3c6ba83df92750f01eb11a8a3639cd89
Sanjarbek9706 <san19970618@gmail.com>
2026-06-09 22:16:04 +0900

    feat: upload files into server via multer

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -2,6 +2,7 @@ import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 import productController from "./controllers/product.controller";
+import makeUploader from "./libs/utils/uploader";
 
 /**  Restaurant */
 routerAdmin.get("/", restaurantController.goHome);
@@ -10,7 +11,8 @@ routerAdmin
 .post("/login", restaurantController.processLogin);
 routerAdmin
 .get("/signup", restaurantController.getSignup)
-.post("/signup", restaurantController.processSignup);
+.post("/signup", makeUploader("members").single("memberImage"),
+ restaurantController.processSignup);
 routerAdmin.get("/logout", restaurantController.logout)
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
@@ -18,8 +20,14 @@ routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 routerAdmin.get("/product/all",
     restaurantController.verifyRestaurant,
     productController.getAllProducts);
-routerAdmin.post("/product/create", productController.createNewProducts);
-routerAdmin.post("/product/:id", productController.updateChosenProducts);
+routerAdmin.post("/product/create",
+     restaurantController.verifyRestaurant,
+     // uploadProductImage.single('productImage'),
+     makeUploader("products").array("productImage", 5),
+     productController.createNewProducts);
+routerAdmin.post("/product/:id",
+     restaurantController.verifyRestaurant,
+     productController.updateChosenProducts);
 /** User */
 
 export default routerAdmin;
