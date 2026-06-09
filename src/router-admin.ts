09aa5df01c1c6b1494b70b098fafc059c5360371
Sanjarbek9706 <san19970618@gmail.com>
2026-06-09 19:44:44 +0900

    feat: locals variable integration

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -15,7 +15,9 @@ routerAdmin.get("/logout", restaurantController.logout)
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
 /** Product */
-routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.get("/product/all",
+    restaurantController.verifyRestaurant,
+    productController.getAllProducts);
 routerAdmin.post("/product/create", productController.createNewProducts);
 routerAdmin.post("/product/:id", productController.updateChosenProducts);
 /** User */
