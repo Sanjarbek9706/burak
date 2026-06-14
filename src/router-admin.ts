ddbb2addc4709d7b1114e56363c6fb3dcbf980a4
Sanjarbek9706 <san19970618@gmail.com>
2026-06-14 21:59:43 +0900

    feat: develop updataChosenUser api

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -32,6 +32,8 @@ routerAdmin.post("/product/:id",
 routerAdmin.get("/user/all",
      restaurantController.verifyRestaurant, 
      restaurantController.getUsers);
-// routerAdmin.post()
+routerAdmin.post("/user/edit", 
+     restaurantController.verifyRestaurant, 
+     restaurantController.updateChosenUser);
 export default routerAdmin;
 
