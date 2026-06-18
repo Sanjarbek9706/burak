6c46c6e2f497950c74df7c31f7594f62494493a3
Sanjarbek9706 <san19970618@gmail.com>
2026-06-19 03:01:45 +0900

    feat: signup  process frontend validation

--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,1 +1,41 @@
-console.log("Signup frontend javascript file");
+console.log(
+  "Signup frontend javascript file",
+);
+
+$(function () {});
+
+function validateSignupForm() {
+  const memberNick = $(
+    ".member-nick",
+  ).val();
+  const memberPhone = $(
+    ".member-phone",
+  ).val();
+  const memberPassword = $(
+    ".member-password",
+  ).val();
+  const confirmPassword = $(
+    ".confirm-password",
+  ).val();
+
+  if (
+    memberNick === "" ||
+    memberPhone === "" ||
+    memberPassword === "" ||
+    confirmPassword === ""
+  ) {
+    alert(
+      "Please fill in all required fields.",
+    );
+    return false;
+  }
+
+  if (
+    memberPassword !== confirmPassword
+  ) {
+    alert(
+      "Passwords differ. Please check your password.",
+    );
+    return false;
+  }
+}
