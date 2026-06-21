5ac0ee10130ddc2f7335f70d1e494b28c62a57eb
Sanjarbek9706 <san19970618@gmail.com>
2026-06-21 19:27:47 +0900

    feat: update chosen product frontend logic

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -37,6 +37,54 @@ $(function () {
       "flex",
     );
   });
+
+  $(".new-product-status").on(
+    "change",
+    async function (e) {
+      const id = e.target.id;
+      const productStatus = $(
+        `#${id}.new-product-status`,
+      ).val();
+      console.log("id:", id);
+      console.log(
+        "productStatus:",
+        productStatus,
+      );
+
+      try {
+        const response =
+          await axios.post(
+            `/admin/product/${id}`,
+            {
+              productStatus:
+                productStatus,
+            },
+          );
+        console.log(
+          "response:",
+          response,
+        );
+        const result = response.data;
+        if (result.data) {
+          console.log(
+            "Product updated!",
+          );
+          $(
+            ".new-product-status",
+          ).blur();
+        } else
+          alert(
+            "Product update failed!",
+          );
+      } catch (err) {
+        console.log(
+          "Error productStatus:",
+          err,
+        );
+        alert("Product update failed!");
+      }
+    },
+  );
 });
 
 function validateForm() {
