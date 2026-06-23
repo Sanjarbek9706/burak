8c97f658da5a4934a601a9aa22da79980548015a
Sanjarbek9706 <san19970618@gmail.com>
2026-06-23 22:46:13 +0900

    fix: admin project refactoring

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,111 +1,56 @@
-console.log(
-  "Products frontend javascript file",
-);
+console.log("Products frontend javascript file");
 
 $(function () {
-  $(".product-collection").on(
-    "change",
-    () => {
-      const selectedValue = $(
-        ".product-collection",
-      ).val();
-      if (selectedValue === "DRINK") {
-        $("#product-collection").hide();
-        $("#product-volume").show();
-      } else {
-        $("#product-volume").hide();
-        $("#product-collection").show();
-      }
-    },
-  );
+  $(".product-collection").on("change", () => {
+    const selectedValue = $(".product-collection").val();
+    if (selectedValue === "DRINK") {
+      $("#product-collection").hide();
+      $("#product-volume").show();
+    } else {
+      $("#product-volume").hide();
+      $("#product-collection").show();
+    }
+  });
   $("#process-btn").on("click", () => {
-    $(".dish-container").slideToggle(
-      500,
-    );
-    $("#process-btn").css(
-      "display",
-      "none",
-    );
+    $(".dish-container").slideToggle(500);
+    $("#process-btn").css("display", "none");
   });
 
   $("#cancel-btn").on("click", () => {
-    $(".dish-container").slideToggle(
-      100,
-    );
-    $("#process-btn").css(
-      "display",
-      "flex",
-    );
+    $(".dish-container").slideToggle(100);
+    $("#process-btn").css("display", "flex");
   });
 
-  $(".new-product-status").on(
-    "change",
-    async function (e) {
-      const id = e.target.id;
-      const productStatus = $(
-        `#${id}.new-product-status`,
-      ).val();
-      console.log("id:", id);
-      console.log(
-        "productStatus:",
-        productStatus,
-      );
+  $(".new-product-status").on("change", async function (e) {
+    const id = e.target.id;
+    const productStatus = $(`#${id}.new-product-status`).val();
+    // console.log("id:", id);
+    // console.log("productStatus:", productStatus);
 
-      try {
-        const response =
-          await axios.post(
-            `/admin/product/${id}`,
-            {
-              productStatus:
-                productStatus,
-            },
-          );
-        console.log(
-          "response:",
-          response,
-        );
-        const result = response.data;
-        if (result.data) {
-          console.log(
-            "Product updated!",
-          );
-          $(
-            ".new-product-status",
-          ).blur();
-        } else
-          alert(
-            "Product update failed!",
-          );
-      } catch (err) {
-        console.log(
-          "Error productStatus:",
-          err,
-        );
-        alert("Product update failed!");
-      }
-    },
-  );
+    try {
+      const response = await axios.post(`/admin/product/${id}`, {
+        productStatus: productStatus,
+      });
+      console.log("response:", response);
+      const result = response.data;
+      if (result.data) {
+        // console.log("Product updated!");
+        $(".new-product-status").blur();
+      } else alert("Product update failed!");
+    } catch (err) {
+      console.log("Error productStatus:", err);
+      alert("Product update failed!");
+    }
+  });
 });
 
 function validateForm() {
-  const productName = $(
-    ".product-name",
-  ).val();
-  const productPrice = $(
-    ".product-price",
-  ).val();
-  const productLeftCount = $(
-    ".product-left-count",
-  ).val();
-  const productCollection = $(
-    ".product-collection",
-  ).val();
-  const productDesc = $(
-    ".product-desc",
-  ).val();
-  const productStatus = $(
-    ".product-status",
-  ).val();
+  const productName = $(".product-name").val(),
+    productPrice = $(".product-price").val(),
+    productLeftCount = $(".product-left-count").val(),
+    productCollection = $(".product-collection").val(),
+    productDesc = $(".product-desc").val(),
+    productStatus = $(".product-status").val();
 
   if (
     productName === "" ||
@@ -120,12 +65,8 @@ function validateForm() {
   } else return true;
 }
 
-function previewFileHandler(
-  input,
-  order,
-) {
-  const imgClassName =
-    input.imgClassName;
+function previewFileHandler(input, order) {
+  const imgClassName = input.imgClassName;
   console.log("input:", input);
   console.log(
     "imgClassName:", // underfined qaytaryabdi?
@@ -135,32 +76,20 @@ function previewFileHandler(
   //   const file = $(
   //     `.${imgClassName}`,
   //   ).get(0).files[0];
-  const file = input.files
-    ? input.files[0]
-    : null;
+  const file = input.files ? input.files[0] : null;
 
   if (!file) return;
 
   const fileType = file["type"];
-  const validImageType = [
-    "image/jpeg",
-    "image/jpg",
-    "image/png",
-  ];
+  const validImageType = ["image/jpeg", "image/jpg", "image/png"];
 
-  if (
-    !validImageType.includes(fileType)
-  ) {
-    alert(
-      "Please insert only jpeg, jpg, and png!",
-    );
+  if (!validImageType.includes(fileType)) {
+    alert("Please insert only jpeg, jpg, and png!");
   } else {
     if (file) {
       const reader = new FileReader();
       reader.onload = function () {
-        $(
-          `#image-section-${order}`,
-        ).attr("src", reader.result);
+        $(`#image-section-${order}`).attr("src", reader.result);
       };
       reader.readAsDataURL(file);
     }
--- src/public/js/signup.js
+++ src/public/js/signup.js
@@ -1,107 +1,50 @@
-console.log(
-  "Signup frontend javascript file",
-);
+console.log("Signup frontend javascript file");
 
 $(function () {
-  const fileTarget = $(
-    ".file-box .upload-hidden",
-  );
+  const fileTarget = $(".file-box .upload-hidden");
   let fileName;
 
   fileTarget.on("change", function () {
     if (window.FileReader) {
-      const uploadedFile =
-        $(this)[0].files[0];
-      console.log(
-        "Uploaded file:",
-        uploadedFile,
-      );
-      const fileType =
-        uploadedFile["type"];
-      const validImageType = [
-        "image/jpeg",
-        "image/jpg",
-        "image/png",
-      ];
-      if (
-        !validImageType.includes(
-          fileType,
-        )
-      ) {
-        alert(
-          "Please insert only jpeg, jpg, and png!",
-        );
+      const uploadedFile = $(this)[0].files[0],
+        // console.log("Uploaded file:", uploadedFile);
+        fileType = uploadedFile["type"],
+        validImageType = ["image/jpeg", "image/jpg", "image/png"];
+      if (!validImageType.includes(fileType)) {
+        alert("Please insert only jpeg, jpg, and png!");
       } else {
         if (uploadedFile) {
-          console.log(
-            URL.createObjectURL(
-              uploadedFile,
-            ),
-          );
-          $(".upload-img-frame")
-            .attr(
-              "src",
-              URL.createObjectURL(
-                uploadedFile,
-              ),
-            )
-            .addClass("success");
+          console.log(URL.createObjectURL(uploadedFile));
+          $(".upload-img-frame").attr("src", URL.createObjectURL(uploadedFile)).addClass("success");
         }
-        filename =
-          $(this)[0].files[0].name;
+        filename = $(this)[0].files[0].name;
       }
-      $(this)
-        .siblings(".upload-name")
-        .val(filename);
+      $(this).siblings(".upload-name").val(filename);
     }
   });
 });
 
 function validateSignupForm() {
-  const memberNick = $(
-    ".member-nick",
-  ).val();
-  const memberPhone = $(
-    ".member-phone",
-  ).val();
-  const memberPassword = $(
-    ".member-password",
-  ).val();
-  const confirmPassword = $(
-    ".confirm-password",
-  ).val();
+  const memberNick = $(".member-nick").val(),
+    memberPhone = $(".member-phone").val(),
+    memberPassword = $(".member-password").val(),
+    confirmPassword = $(".confirm-password").val();
 
-  if (
-    memberNick === "" ||
-    memberPhone === "" ||
-    memberPassword === "" ||
-    confirmPassword === ""
-  ) {
-    alert(
-      "Please fill in all required fields.",
-    );
+  if (memberNick === "" || memberPhone === "" || memberPassword === "" || confirmPassword === "") {
+    alert("Please fill in all required fields.");
     return false;
   }
 
-  if (
-    memberPassword !== confirmPassword
-  ) {
-    alert(
-      "Passwords differ. Please check your password.",
-    );
+  if (memberPassword !== confirmPassword) {
+    alert("Passwords differ. Please check your password.");
     return false;
   }
 
-  const memberImage = $(
-    ".member-image",
-  ).get(0).files[0]
-    ? $(".member-image").get(0).files[0]
-        .name
+  const memberImage = $(".member-image").get(0).files[0]
+    ? $(".member-image").get(0).files[0].name
     : null;
   if (!memberImage) {
-    alert(
-      "Please insert restaurant image!",
-    );
+    alert("Please insert restaurant image!");
     return false;
   }
 }
