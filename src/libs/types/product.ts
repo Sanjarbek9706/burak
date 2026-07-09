3fee25eea03923bcd66f2e414df0a45ad6eb30c2
Sanjarbek9706 <san19970618@gmail.com>
2026-07-09 21:22:11 +0900

    feat: develop getProduct rest Api

--- src/libs/types/product.ts
+++ src/libs/types/product.ts
@@ -19,6 +19,14 @@ import {
     productViews: number;
 }
 
+export interface ProductInquiry {
+    order: string;
+    page: number;
+    limit: number;
+    productCollection?: ProductCollection;
+    search?: string;
+}
+
 export interface ProductInput {
     productStatus?: ProductStatus;
     productCollection: ProductCollection;
--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -1,7 +1,10 @@
 import ProductModel from "../schema/Product.model";
-import { Product, ProductInput, ProductUpdateInput } from "../libs/types/product";
+import { Product, ProductInput, ProductInquiry, ProductUpdateInput } from "../libs/types/product";
 import Errors, { HttpCode, Message } from "../libs/types/Errors";
 import { shapeIntoMongooseObjectId } from "../libs/config";
+import { ProductStatus } from "../libs/enums/product.enam";
+import { T } from "../libs/types/common";
+import { ObjectId } from "mongoose";
 
 class ProductService {
     
@@ -12,6 +15,50 @@ class ProductService {
     }
 
 /* SPA */
+
+public async getProducts(inquiry: ProductInquiry): Promise<Product[]> {
+    console.log("inquiry:", inquiry);
+    const match: T = {productStatus: ProductStatus.PROCESS};
+    if (inquiry.productCollection) 
+        match.productCollection = inquiry.productCollection;
+    if (inquiry.search) {
+        match.productName = { $regex: new RegExp(inquiry.search, "i") };
+    }
+
+    const sort: T = inquiry.order === "productPrice" 
+    ? { [inquiry.order]: 1 } 
+    : { [inquiry.order]: -1 };
+
+    const result = await this.productModel.aggregate([
+        { $match: match },
+        { $sort: sort },
+        { $skip: (inquiry.page *1 - 1) * inquiry.limit }, // 3  X 1 2 3
+        { $limit: inquiry.limit  * 1 },                   // 3   => 4 5 6
+    ])
+    .exec();
+    if(!result) throw  new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    return result;
+    }
+
+
+ public async getProduct(memberId: ObjectId | null, 
+    id: string
+    ): Promise<Product> {
+    const productId = shapeIntoMongooseObjectId(id);
+
+    let result = await this.productModel
+    .findOne({ _id: productId, productStatus: ProductStatus.PROCESS 
+    })
+    .exec();
+    
+    if(!result) throw  new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
+
+    //TODO: If authenticated users => first => view log creation
+
+    return result;
+ }
+
 
 /** SSR */
 
