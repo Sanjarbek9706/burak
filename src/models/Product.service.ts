d11fcea34f8e18922ed81d8e36909c92ff85cf59
Sanjarbek9706 <san19970618@gmail.com>
2026-07-09 23:18:24 +0900

    fix: modify getProduct business logic

--- src/models/Product.service.ts
+++ src/models/Product.service.ts
@@ -5,13 +5,18 @@ import { shapeIntoMongooseObjectId } from "../libs/config";
 import { ProductStatus } from "../libs/enums/product.enam";
 import { T } from "../libs/types/common";
 import { ObjectId } from "mongoose";
+import ViewService from "./View.service";
+import { ViewInput } from "../libs/types/view";
+import { ViewGroup } from "../libs/enums/view.enum";
 
 class ProductService {
     
     private readonly productModel;
+    public viewService;
 
     constructor() {
         this.productModel = ProductModel;
+        this.viewService = new ViewService()
     }
 
 /* SPA */
@@ -55,6 +60,30 @@ public async getProducts(inquiry: ProductInquiry): Promise<Product[]> {
     if(!result) throw  new Errors(HttpCode.NOT_FOUND, Message.NO_DATA_FOUND);
 
     //TODO: If authenticated users => first => view log creation
+    if(memberId) {
+        // Check Existence
+       const input: ViewInput = {
+        memberId: memberId,
+        viewRefId: productId,
+        viewGroup: ViewGroup.PRODUCT,
+       }
+       const existView = await this.viewService.checkViewExistence(input);
+
+       console.log("exist:", !!existView);
+        if(!existView) {
+       // Insert View 
+            await this.viewService.insertMerberView(input);
+        
+        // Increase Counts
+       result = await this.productModel
+        .findByIdAndUpdate(productId, 
+            {$inc: {productViews: +1 } },
+            {new: true}
+        )
+        .exec();
+    }
+ }
+
 
     return result;
  }
--- src/models/View.service.ts
+++ src/models/View.service.ts
@@ -1,3 +1,5 @@
+import Errors, { HttpCode, Message } from "../libs/types/Errors";
+import { View, ViewInput } from "../libs/types/view";
 import VeiwModel from "../schema/Veiw.model";
 
 class ViewService {
@@ -5,6 +7,21 @@ class ViewService {
 
     constructor() {
         this.viewModel = VeiwModel;
+    }
+
+    public async checkViewExistence(input: ViewInput): Promise<View> {
+        return await this.viewModel
+        .findOne({memberId: input.memberId,viewRefId: input.viewRefId})
+        .exec();
+    }
+    
+    public async insertMerberView(input: ViewInput): Promise<View> {
+        try {
+            return await this.viewModel.create(input);
+        } catch (err) {
+            console.log("ERROR, model: insertMemberView:", err);
+            throw new Errors(HttpCode.BAD_REQUEST, Message.CREATE_FAILED);
+        }
     }
 }
 
