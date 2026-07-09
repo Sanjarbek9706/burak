0911603a3cd055cd8dc7c218093ce14997c7a449
Sanjarbek9706 <san19970618@gmail.com>
2026-07-09 21:55:17 +0900

    feat: creat view schema and service models

---
+++ src/models/View.service.ts
@@ -0,0 +1,11 @@
+import VeiwModel from "../schema/Veiw.model";
+
+class ViewService {
+    private readonly viewModel;
+
+    constructor() {
+        this.viewModel = VeiwModel;
+    }
+}
+
+export default ViewService; 
