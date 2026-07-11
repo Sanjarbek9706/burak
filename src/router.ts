c7ed8c663cca7b53ab28cdaa8ef252a7e78c2a20
Sanjarbek9706 <san19970618@gmail.com>
2026-07-11 21:54:41 +0900

    feat: develop getMyOrders rest Api'

--- src/router.ts
+++ src/router.ts
@@ -36,6 +36,10 @@ router.post(
     memberController.verifyAuth,
     orderController.createOrder
 );
+router.get("/order/all", 
+    memberController.verifyAuth,
+    orderController.getMyOrders
+);
 
 export default router;
 
